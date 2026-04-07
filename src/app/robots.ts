e4e574b128647c5cde155622b77a1073849839c5
Sachin Bind <bindsachin438@gmail.com>
2026-04-07 22:46:39 +0530

    Update robots.txt rules to allow specific paths

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,11 +5,10 @@ export default function robots(): MetadataRoute.Robots {
     rules: [
       {
         userAgent: '*',
-        allow: '/',
+        allow: ['/', '/_next/static/', '/_next/image/'],
         disallow: [
           '/admin/',
           '/api/',
-          '/_next/',
         ],
       },
       {
